de2bf3194a4865e70ad54c09a1a822a74203e8ab
lim <lim@192.168.123.144>
2020-09-10 23:57:08 +0900

    part5 final

--- ex04/src/main/java/org/zerock/service/SampleServiceImpl.java
+++ ex04/src/main/java/org/zerock/service/SampleServiceImpl.java
@@ -7,8 +7,11 @@ public class SampleServiceImpl implements SampleService {
 
 	@Override
 	public Integer doAdd(String str1, String str2) throws Exception {
-		// TODO Auto-generated method stub
+
 		return Integer.parseInt(str1) + Integer.parseInt(str2);
+
 	}
 
 }
+
+
